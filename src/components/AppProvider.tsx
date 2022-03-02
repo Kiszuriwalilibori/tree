2811baa4d72e9e05e46729e267bd6fd356ff1e02
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2022-03-02 17:43:37 +0100

    persistent store added

--- src/components/AppProvider.tsx
+++ src/components/AppProvider.tsx
@@ -3,12 +3,49 @@ import { Provider } from 'react-redux';
 import '../styles/App.css';
 import itemsReducer from '../redux/reducers/itemsReducer';
 import inputsReducer from '../redux/reducers/inputReducer';
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
-export const store = configureStore({ reducer: { items: itemsReducer, input: inputsReducer } });
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+};
+const rootReducer = combineReducers({ items: itemsReducer, input: inputsReducer });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+
+let persistor = persistStore(store);
 
 const AppProvider: React.FC = ({ children }) => {
-    return <Provider store={store}>{children}</Provider>;
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                {children}
+            </PersistGate>
+        </Provider>
+    );
 };
+
 export default AppProvider;
 export type RootStateType = ReturnType<typeof store.getState>;
