34911ef75729c40b221032057815500e29c495ae
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2022-08-26 10:08:48 +0200

    few files and vars renamed

--- src/HOCs/renderWhenVisible.tsx
+++ src/HOCs/renderWhenVisible.tsx
@@ -2,11 +2,11 @@ interface isVisible {
     isVisible: boolean;
 }
 
-function RenderWhenIsVisible(Component: React.ComponentType<isVisible | any>) {
+function renderWhenIsVisible(Component: React.ComponentType<isVisible | any>) {
     return function (props: isVisible | any) {
         let { isVisible, ...newProps } = props;
         return props.isVisible ? <Component {...newProps} /> : null;
     };
 }
 
-export default RenderWhenIsVisible;
+export default renderWhenIsVisible;
